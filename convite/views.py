6fb27de94fd3d007491f5423a4575aa56dba875e
AlefIFSuldeMinas <12191000382@muz.ifsuldeminas.edu.br>
2022-05-23 20:39:54 -0300

    Atualização projeto

--- convite/views.py
+++ convite/views.py
@@ -1,3 +1,20 @@
+from convite.models import ConviteCasamento
 from django.shortcuts import render
 
-# Create your views here.
+def CadastroConvite(request):
+    context = {}
+    if request.method == "POST":
+        form = ConviteCasamento(request.POST, request.FILES)
+        if form.is_valid():
+            name = form.cleaned_data.get("name")
+            img = form.cleaned_data.get("geeks_field")
+            obj = ConviteCasamento.objects.create(
+                                 title = name,
+                                 img = img
+                                 )
+            obj.save()
+            print(obj)
+    else:
+        form = ConviteCasamento()
+    context['form']= form
+    return render(request, "convite\convite_casamento.html", context)
