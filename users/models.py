af7b6c0f362a81f619287d18262f0bd7bb148725
AlefIFSuldeMinas <12191000382@muz.ifsuldeminas.edu.br>
2022-06-13 22:13:54 -0300

    Tela de cadastro usuário finalizada

--- users/models.py
+++ users/models.py
@@ -2,4 +2,10 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class User(AbstractUser):
-    GrupoUsuario = models.TextField(blank=True)
+    NomeUsuario = models.TextField(blank=True)
+    Endereco = models.TextField(blank=True)
+    Celular = models.TextField(blank=True)
+    Cidade = models.TextField(blank=True)
+    Estado = models.TextField(blank=True)
+    Cep = models.TextField(blank=True)
+    Bairro = models.TextField(blank=True)
--- users/views.py
+++ users/views.py
@@ -5,6 +5,8 @@ from django.views.generic import TemplateView
 from django.contrib.auth import authenticate
 from django.contrib.auth import login as loginDjango
 from users.models import User
+import logging
+
 
 def cadastro(request):
     if request.method == "GET":
@@ -13,28 +15,50 @@ def cadastro(request):
         NomeUsuario = request.POST.get('NomeUsuario')
         Email = request.POST.get('Email')
         Senha = request.POST.get('Senha')
-        user = User.objects.create_user(username=NomeUsuario,email=Email,password=Senha)
+        Endereco = request.POST.get('Endereco')
+        Celular = request.POST.get('Celular')
+        Cidade = request.POST.get('Cidade')
+        Estado = request.POST.get('Estado')
+        Cep = request.POST.get('Cep')
+        Bairro = request.POST.get('Bairro')
+
+        user = User.objects.create_user(
+            username=Email,
+            password=Senha,
+            NomeUsuario=NomeUsuario,          
+            Endereco=Endereco,
+            Celular=Celular,
+            Cidade=Cidade,
+            Estado=Estado,
+            Cep=Cep,
+            Bairro=Bairro
+        )
         user.save()
-        return HttpResponse(NomeUsuario)
+        return render(request, 'users/login.html')
+
 
 def login(request):
     if request.method == "GET":
         return render(request, 'users/login.html')
     else:
-        NomeUsuario = request.POST.get('NomeUsuario')
+        Email = request.POST.get('Email')
         Senha = request.POST.get('Senha')
-        user = authenticate(username=NomeUsuario, password=Senha)
+        print(Senha, Email)
+        user = authenticate(username=Email, password=Senha)
+
         if user:
             loginDjango(request, user)
             return HttpResponse("Logado com sucesso!")
         else:
             return HttpResponse('Senha errada')
 
+
 def site(request):
     if request.user.is_authenticated:
         return HttpResponse('Logado')
     else:
         return HttpResponse('Erroooou!')
-        
+
+
 class HomeView(TemplateView):
     template_name = 'users/index.html'
